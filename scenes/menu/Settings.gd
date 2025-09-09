5f2d0f9b6182eb7a7c7448836dd625b59b14aee7
Nordup <nordup.ondr@gmail.com>
2025-09-10 04:31:56 +0700

    small fixes

--- scenes/menu/Settings.gd
+++ scenes/menu/Settings.gd
@@ -1,15 +1,12 @@
 extends Control
 
 signal resume
-
-@onready var _xr = Util.is_xr()
 @onready var _vbox = $ScrollContainer/MarginContainer/VBoxContainer/MarginContainer
 @onready var _tab_bar = $ScrollContainer/MarginContainer/VBoxContainer/TabBar
 @onready var _tab_scenes = [
   _vbox.get_node("GraphicsSettings"),
   _vbox.get_node("AudioSettings"),
-  _vbox.get_node("XrControlsMenu") if _xr else null,
-  _vbox.get_node("ControlSettings") if not _xr else null,
+  _vbox.get_node("ControlSettings"),
   _vbox.get_node("DataSettings") if not Util.is_web() else null,
 ]
 
